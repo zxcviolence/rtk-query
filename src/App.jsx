ceffe5efe81854334d15c3603b1ee31214bd7559
Islam <yakubov959@mail.ru>
2023-12-15 15:13:51 +0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
-import { useGetTodosQuery } from "./redux/todosApi";
+import {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useDeleteTodoMutation,
+} from "./redux/todosApi";
+import { useState } from "react";
 
 function App() {
-  const { data = [], isLoading } = useGetTodosQuery();
+  const [count, setCount] = useState("");
+  const [newTodo, setNewTodo] = useState("");
+  const { data = [], isLoading } = useGetTodosQuery(count);
+  const [addTodo] = useAddTodoMutation(); // При необходимости можно достать статус загрузки, ошибки и т.д.
+  const [deleteTodo] = useDeleteTodoMutation();
+
+  const handleAddNewTodo = async () => {
+    if (newTodo) {
+      await addTodo({ text: newTodo }).unwrap();
+    }
+    setNewTodo("");
+  };
+
+  const handleDeleteTodo = async id => {
+    await deleteTodo(id).unwrap();
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      {isLoading && <div>Loading...</div>}
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={e => setNewTodo(e.target.value)}
+        />
+        <button onClick={handleAddNewTodo}>Add todo</button>
+      </div>
+      <div>
+        <select value={count} onChange={e => setCount(e.target.value)}>
+          <option value="">all</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
       <ul>
         {data.map(item => (
-          <li key={item.id}>{item.text}</li>
+          <div key={item.id}>
+            <li>{item.text}</li>
+            <button onClick={() => handleDeleteTodo(item.id)}>Remove</button>
+          </div>
         ))}
       </ul>
     </div>
--- src/redux/todosApi.js
+++ src/redux/todosApi.js
@@ -2,12 +2,43 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const todosApi = createApi({
   reducerPath: "todosApi",
+  // Мы работаем здесь с тудушками
+  tagTypes: ["todos"],
+  // Основной Url, по которому будут выполняться запросы. Дальше в endpoints расширяем
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
   endpoints: build => ({
+    // Главный entrypoint на получение данных
     getTodos: build.query({
-      query: () => "todos",
+      query: (limit = "") => `todos?${limit && `_limit=${limit}`}`,
+      // Обеспечиваем здесь некие тэги, для каждой тудушки добавляем его уникальный id, указываем тип, который указан в tagTypes,
+      // указываем что все это будет списком этой категории
+      providesTags: result =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "todos", id })),
+              { type: "todos", id: "LIST" },
+            ]
+          : [{ type: "todos", id: "LIST" }],
+    }),
+    addTodo: build.mutation({
+      query: body => ({
+        url: "todos",
+        method: "POST",
+        body,
+      }),
+      // После того как выполним мутацию, указываем что у нас изменилось (для автоматического обновления данных)
+      invalidatesTags: [{ type: "todos", id: "LIST" }],
+    }),
+    deleteTodo: build.mutation({
+      query: id => ({
+        url: `todos/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [{ type: "todos", id: "LIST" }],
     }),
   }),
 });
 
-export const { useGetTodosQuery } = todosApi;
+// хуки, создаются автоматически
+export const { useGetTodosQuery, useAddTodoMutation, useDeleteTodoMutation } =
+  todosApi;
